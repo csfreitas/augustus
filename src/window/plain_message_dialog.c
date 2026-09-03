3fd02a8109765fbfcb21be34691e568d4bc49c92
csfreitas <kssiofreitas@gmail.com>
2026-09-03 18:18:50 -0300

    Make translated UI layouts adapt to their content

--- src/window/plain_message_dialog.c
+++ src/window/plain_message_dialog.c
@@ -8,6 +8,13 @@
 #include "graphics/text.h"
 #include "graphics/window.h"
 #include "input/input.h"
+#include "window/message_dialog_layout.h"
+
+#define MINIMUM_DIALOG_WIDTH 480
+#define MINIMUM_DIALOG_HEIGHT 208
+#define MESSAGE_FOOTER_HEIGHT 49
+#define EXTRA_TEXT_TOP_MARGIN 10
+#define EXTRA_TEXT_SPACING 30
 
 static void button_ok(int param1, int param2);
 
@@ -22,7 +29,7 @@ static struct {
     const uint8_t *title;
     const uint8_t *message;
     int draw_underlying_window;
-    int height;
+    message_dialog_layout layout;
     struct {
         const uint8_t **texts;
         unsigned int total_texts;
@@ -37,19 +44,29 @@ static int init(translation_key title, translation_key message, int should_draw_
         // don't show popup over popup
         return 0;
     }
+    if (!texts) {
+        num_texts = 0;
+    }
     data.title = translation_for(title);
     data.message = translation_for(message);
     data.draw_underlying_window = should_draw_underlying_window;
     data.extra.texts = texts;
     data.extra.is_list = texts_as_list;
     data.extra.total_texts = num_texts;
-    data.height = 13;
-    if (num_texts) {
-        if (num_texts > 2) {
-            data.height += ((num_texts - 2) * 30 + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    int preferred_width = MINIMUM_DIALOG_WIDTH;
+    for (unsigned int i = 0; i < num_texts; i++) {
+        const uint8_t *extra_text = texts[i] ? texts[i] : EMPTY_STRING;
+        int extra_width = text_get_width(extra_text, FONT_NORMAL_BLACK) + 40;
+        if (extra_width > preferred_width) {
+            preferred_width = extra_width;
         }
     }
-    buttons[0].y_offset = data.height * (BLOCK_SIZE - 1) - buttons[0].height;
+    int extra_height = num_texts ? EXTRA_TEXT_TOP_MARGIN + (num_texts - 1) * EXTRA_TEXT_SPACING +
+        font_definition_for(FONT_NORMAL_BLACK)->line_height : 0;
+    message_dialog_layout_calculate(&data.layout, data.title, data.message, preferred_width,
+        MINIMUM_DIALOG_WIDTH, MINIMUM_DIALOG_HEIGHT, MESSAGE_FOOTER_HEIGHT + extra_height);
+    buttons[0].x_offset = (data.layout.width - buttons[0].width) / 2;
+    buttons[0].y_offset = data.layout.height - buttons[0].height - 13;
     return 1;
 }
 
@@ -59,19 +76,26 @@ static void draw_background(void)
         window_draw_underlying_window();
     }
 
-    graphics_in_dialog();
-    outer_panel_draw(80, 80, 30, data.height);
-    text_draw_centered(data.title, 80, 100, 480, FONT_LARGE_BLACK, 0);
-    int y_offset = 150 + text_draw_multiline(data.message, 100, 140, 450, 0, FONT_NORMAL_BLACK, 0);
+    graphics_in_dialog_with_size(data.layout.width, data.layout.height);
+    outer_panel_draw(0, 0, data.layout.width / BLOCK_SIZE, data.layout.height / BLOCK_SIZE);
+    text_draw_multiline(data.title, data.layout.title_x, data.layout.title_y,
+        data.layout.title_width, 1, FONT_LARGE_BLACK, 0);
+    text_draw_multiline(data.message, data.layout.message_x, data.layout.message_y,
+        data.layout.message_width, 0, FONT_NORMAL_BLACK, 0);
+    int y_offset = data.layout.message_y + data.layout.message_height + EXTRA_TEXT_TOP_MARGIN;
     if (data.extra.total_texts) {
         for (unsigned int i = 0; i < data.extra.total_texts; i++) {
+            const uint8_t *extra_text = data.extra.texts[i] ? data.extra.texts[i] : EMPTY_STRING;
             if (!data.extra.is_list) {
-                text_draw_centered(data.extra.texts[i], 100, y_offset, 450, FONT_NORMAL_BLACK, 0);
+                text_draw_centered(extra_text, data.layout.message_x, y_offset,
+                    data.layout.message_width, FONT_NORMAL_BLACK, 0);
             } else {
-                int width = text_draw(string_from_ascii("-"), 100, y_offset, FONT_NORMAL_BLACK, 0);
-                text_draw(data.extra.texts[i], 100 + width, y_offset, FONT_NORMAL_BLACK, 0);
+                int width = text_draw(string_from_ascii("-"), data.layout.message_x, y_offset,
+                    FONT_NORMAL_BLACK, 0);
+                text_draw(extra_text, data.layout.message_x + width, y_offset,
+                    FONT_NORMAL_BLACK, 0);
             }
-            y_offset += 30;
+            y_offset += EXTRA_TEXT_SPACING;
         }
     }
     graphics_reset_dialog();
@@ -79,8 +103,8 @@ static void draw_background(void)
 
 static void draw_foreground(void)
 {
-    graphics_in_dialog();
-    image_buttons_draw(80, 80, buttons, 1);
+    graphics_in_dialog_with_size(data.layout.width, data.layout.height);
+    image_buttons_draw(0, 0, buttons, 1);
     graphics_reset_dialog();
 }
 
@@ -91,7 +115,8 @@ static void close(void)
 
 static void handle_input(const mouse *m, const hotkeys *h)
 {
-    if (image_buttons_handle_mouse(mouse_in_dialog(m), 80, 80, buttons, 1, 0)) {
+    const mouse *m_dialog = mouse_in_dialog_with_size(m, data.layout.width, data.layout.height);
+    if (image_buttons_handle_mouse(m_dialog, 0, 0, buttons, 1, 0)) {
         return;
     }
     if (input_go_back_requested(m, h) || h->enter_pressed) {
